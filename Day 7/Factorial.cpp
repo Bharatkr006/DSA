99efb65d15746c7faa5f8ffffa450f1d7f558280
BharatKr006 <itzbharatkr@gmail.com>
2026-01-13 23:50:00 +0530

    Update

--- Day 7/Factorial.cpp
+++ Day 7/Factorial.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 using namespace std;
+
+int fact(int n) {
+    int f = 1;
+    for (int i = n; i > 1; i--) {
+        f = f * i;
+    }
+
+    cout << "The Factorial is : " << f;
+    return f;
+}
+
+int main() {
+    int n;
+    cout << "Enter integer to calculate Factorial :";
+    cin >> n;
+
+    if (n == 0 || n == 1) {
+        cout << "The Factorial is : 1";
+    }
+    else {
+        fact(n);
+    }
+}
