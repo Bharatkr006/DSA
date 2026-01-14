7fd483dc5efec2870651d6045908c9a4c08e5517
BharatKr006 <itzbharatkr@gmail.com>
2026-01-14 19:03:18 +0530

    eCompleted Day 8

--- Day 8/B2D.cpp
+++ Day 8/B2D.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
-    cout << "Hello";
+    int n;
+    int m;
+    int exp = 1;
+    int ans = 0;
+    cout << "Enter Binary number :";
+    cin >> n;
+
+    for (int i = 0; n > 0; i++) {
+        m = n%10;
+        ans = ans + m*exp;
+        n = n / 10;
+        exp *= 2;
+    }
+
+    cout << ans;
     return 0;
 }
---
+++ Day 8/D2B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n,m;
+    int dec = 1;
+    int ans = 0;
+    cout << "Enter Decimal number : " ;
+    cin >> n;
+
+    for (int i = 0; n > 0; i++) {
+        m = n%2;
+        ans = ans + m*dec;
+        dec *= 10;
+        n = n / 2;
+    }
+    cout << ans;
+    return 0;
+}
